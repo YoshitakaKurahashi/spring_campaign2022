f3359ae49b561afbb08231f4ac954a5a9170e43f
YoshitakaKurahashi <72341088+YoshitakaKurahashi@users.noreply.github.com>
2021-12-19 17:27:52 +0900

    pc完成20211219

--- src/js/script.js
+++ src/js/script.js
@@ -15,7 +15,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
   // ボタンの表示設定
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 70) {
+    if ($(this).scrollTop() > 600) {
       // 指定px以上のスクロールでボタンを表示
       topBtn.fadeIn();
     } else {
@@ -44,6 +44,142 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     let targetY = target.offset().top - header;
     $('html,body').animate({ scrollTop: targetY }, time, 'swing');
     return false;
+  });
+
+  // tab切り替え
+  $(function(){
+    $('.js-tab-trigger').on('click', function (e) {
+        // すべてのタブメニューの'is-active'クラスを削除
+        $('.js-tab-trigger').removeClass('is-active');
+        // クリックしたタブメニューに'is-active'クラスを追加
+        $(this).addClass('is-active');
+
+        // ターゲットとなる要素（タブメニューdata属性値と等しいid値を持つコンテンツ要素[ターゲット要素]）を取得
+        let currentContent = $('#' + $(this).data('id'));
+
+        // タブコンテンツを非アクティブにする
+        $('.js-tab-target').removeClass('is-active');
+
+        // 対象コンテンツ(指定したIDの要素があったら)を表示させる
+        if (currentContent.length) {
+            currentContent.addClass('is-active');
+        }
+    });
+  });
+
+  // スワイパー1
+  var sliderThumbnail = new Swiper('.slider-thumbnail01', {
+    slidesPerView: 5,
+    spaceBetween: 17,
+    freeMode: true,
+    autoHeight: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  });
+  
+  //スライダー
+  var slider = new Swiper('.slider01', {
+    spaceBetween: 80,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: sliderThumbnail
+    }
+  });
+
+  // スワイパー2
+  var sliderThumbnail = new Swiper('.slider-thumbnail02', {
+    slidesPerView: 5,
+    spaceBetween: 17,
+    freeMode: true,
+    autoHeight: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  });
+  
+  //スライダー
+  var slider = new Swiper('.slider02', {
+    spaceBetween: 80,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: sliderThumbnail
+    }
+  });
+
+  // スワイパー3
+  var sliderThumbnail = new Swiper('.slider-thumbnail03', {
+    slidesPerView: 5,
+    spaceBetween: 17,
+    freeMode: true,
+    autoHeight: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  });
+  
+  //スライダー
+  var slider = new Swiper('.slider03', {
+    spaceBetween: 80,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: sliderThumbnail
+    }
+  });
+
+  // スワイパー4
+  var sliderThumbnail = new Swiper('.slider-thumbnail04', {
+    slidesPerView: 5,
+    spaceBetween: 17,
+    freeMode: true,
+    autoHeight: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  });
+  
+  //スライダー
+  var slider = new Swiper('.slider04', {
+    spaceBetween: 80,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: sliderThumbnail
+    }
+  });
+
+  // スワイパー5
+  var sliderThumbnail = new Swiper('.slider-thumbnail05', {
+    slidesPerView: 5,
+    spaceBetween: 17,
+    freeMode: true,
+    autoHeight: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  });
+  
+  //スライダー
+  var slider = new Swiper('.slider05', {
+    spaceBetween: 80,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: sliderThumbnail
+    }
   });
 
 });
